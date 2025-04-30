a2e809fc3ac84c1c30029cf982efe5cecbe4b2a3
Leyukezer Cruz <51835032+Leyukezer-Sys@users.noreply.github.com>
2025-04-29 21:04:44 -0400

    ook

---
+++ Atividade29-04-2025/AppMultas/AppMultas/Context/ContextBD.cs
@@ -0,0 +1,14 @@
+﻿using AppMultas.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppMultas.Context
+{
+    public class ContextBD : DbContext
+    {
+        public ContextBD(DbContextOptions<ContextBD> options): base(options) { }
+
+        public DbSet<Veiculo> Veiculos { get; set; }
+
+        public DbSet<Multa> Multas { get; set; }
+    }
+}
