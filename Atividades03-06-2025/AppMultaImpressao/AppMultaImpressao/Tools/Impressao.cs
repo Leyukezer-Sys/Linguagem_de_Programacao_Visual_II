394c09b8a258946752fd52e609e41e5b1c970e3e
Leyukezer Cruz <51835032+Leyukezer-Sys@users.noreply.github.com>
2025-06-03 20:40:19 -0400

    ok

---
+++ Atividades03-06-2025/AppMultaImpressao/AppMultaImpressao/Tools/Impressao.cs
@@ -0,0 +1,108 @@
+﻿using AppMulta.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace AppMultaImpressao.Tools
+{
+    public class Impressao
+    {
+        //método modelo de impressão
+        public async Task GerarImpressaoVeiculos(List<Veiculo> lista1, NavigationManager nav, IJSRuntime jsRuntime)
+        {
+            try
+            {
+                var date = DateTime.Now;
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), @$"Relatorios\VeiculoRelatorio.frx");
+                if (!File.Exists(filePath))
+                {
+                    var report = new FastReport.Report();
+                    report.Dictionary.RegisterBusinessObject(lista1, "lista1", 10, true);
+                    report.Report.Save(filePath);
+
+                }
+
+                var report1 = new FastReport.Report();
+                report1.Report.Load(filePath);
+                report1.Dictionary.RegisterBusinessObject(lista1, "lista1", 10, true);
+                report1.Prepare();
+                using var pdfExport = new FastReport.Export.PdfSimple.PDFSimpleExport();
+                using var reportStream = new MemoryStream();
+                pdfExport.Export(report1, reportStream);
+
+                var fileName = $"Relatorio_{Guid.NewGuid()}.pdf";
+                var filePathTemp = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "RelatoriosTemp", fileName);
+
+                // Cria a pasta se não existir
+                Directory.CreateDirectory(Path.GetDirectoryName(filePathTemp));
+
+                // Salva o arquivo PDF temporariamente na pasta wwwroot/RelatoriosTemp
+                File.WriteAllBytes(filePathTemp, reportStream.ToArray());
+
+                // Gera a URL para o arquivo temporário
+                var url = nav.ToAbsoluteUri($"/RelatoriosTemp/{fileName}");
+
+                //// Abre o relatório em uma nova guia
+                //nav.NavigateTo(url.ToString(), true);
+
+                await jsRuntime.InvokeVoidAsync("NovaGuia", url.ToString()); //alteração 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro aqui!: " + ex.Message.ToString());
+                throw new Exception(ex.Message);
+
+            }
+
+        }//fim do método modelo de impressão 
+
+
+        //método modelo de impressão
+        public async Task GerarImpressaoMultas(List<Multa> listaMulta, NavigationManager nav, IJSRuntime jsRuntime)
+        {
+            try
+            {
+                var date = DateTime.Now;
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), @$"Relatorios\MultaRelatorio.frx");
+                if (!File.Exists(filePath))
+                {
+                    var report = new FastReport.Report();
+                    report.Dictionary.RegisterBusinessObject(listaMulta, "listaMulta", 10, true);
+                    report.Report.Save(filePath);
+
+                }
+
+                var report1 = new FastReport.Report();
+                report1.Report.Load(filePath);
+                report1.Dictionary.RegisterBusinessObject(listaMulta, "listaMulta", 10, true);
+                report1.Prepare();
+                using var pdfExport = new FastReport.Export.PdfSimple.PDFSimpleExport();
+                using var reportStream = new MemoryStream();
+                pdfExport.Export(report1, reportStream);
+
+                var fileName = $"Relatorio_{Guid.NewGuid()}.pdf";
+                var filePathTemp = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "RelatoriosTemp", fileName);
+
+                // Cria a pasta se não existir
+                Directory.CreateDirectory(Path.GetDirectoryName(filePathTemp));
+
+                // Salva o arquivo PDF temporariamente na pasta wwwroot/RelatoriosTemp
+                File.WriteAllBytes(filePathTemp, reportStream.ToArray());
+
+                // Gera a URL para o arquivo temporário
+                var url = nav.ToAbsoluteUri($"/RelatoriosTemp/{fileName}");
+
+                //// Abre o relatório em uma nova guia
+                //nav.NavigateTo(url.ToString(), true);
+
+                await jsRuntime.InvokeVoidAsync("NovaGuia", url.ToString()); //alteração 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro aqui!: " + ex.Message.ToString());
+                throw new Exception(ex.Message);
+
+            }
+
+        }//fim do método modelo de impressão 
+    }
+}
